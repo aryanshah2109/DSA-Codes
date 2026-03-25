03661b25f0897ed75d47e4371f067c01d6ed3142
Aryan Shah <aryanrshah2109@gmail.com>
2026-03-25 22:17:30 +0530

    Stack - 25/3/26

---
+++ Stack/Practical/asteroidsCollision.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def asteroidCollision(self, asteroids):
+        """
+        :type asteroids: List[int]
+        :rtype: List[int]
+        """
+        # TC = O(2n) SC = O(n)
+
+        n = len(asteroids)
+
+        stack = []
+
+        for i in range(n):
+            if asteroids[i] > 0:
+                stack.append(asteroids[i])
+                continue
+            else:
+                while len(stack) > 0 and stack[-1] > 0 and -asteroids[i] > stack[-1]:
+                    stack.pop(-1)
+                if len(stack) > 0 and stack[-1] == -asteroids[i]:
+                    stack.pop(-1)
+
+                elif len(stack) == 0 or stack[-1] < 0:
+                    stack.append(asteroids[i])
+                
+
+        return stack
