7640ec7b8218efc935b69abc7e9dab081211ad96
Aryan Shah <aryanrshah2109@gmail.com>
2026-04-27 12:06:23 +0530

    DSA - Backtracking - 27/04/26

---
+++ BackTracking/Problems/subsets.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def getSubsets(self, nums, answer, curr_subset, idx, n):
+        if idx == n:
+            answer.append(curr_subset[:])
+            return
+        
+        # include element
+        curr_subset.append(nums[idx])
+        self.getSubsets(nums, answer, curr_subset, idx+1, n)
+
+        # backtrack
+        curr_subset.pop(-1)
+
+        # exclude element        
+        self.getSubsets(nums, answer, curr_subset, idx+1, n)
+
+        return answer
+
+
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        ## TC = O(n) * O(2^n)
+        ## SC = O(n) * O(2^n)
+
+        all_subsets = self.getSubsets(nums, [], [], 0, len(nums))
+        return all_subsets
+
+        
---
+++ BackTracking/Problems/subsets_with_duplicates.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def getAllSubsets(self, nums, answer, curr_subset, idx):
+        if idx == len(nums):
+            answer.append(curr_subset[:])
+            return
+        
+        # include element
+        curr_subset.append(nums[idx])
+        self.getAllSubsets(nums, answer, curr_subset, idx+1)
+
+        # backtrack
+        curr_subset.pop(-1)
+
+        # exclude element
+
+        # directly go to next unique element and skip all recurring duplicates
+        i = idx + 1
+        while i < len(nums) and nums[i] == nums[i-1]:
+            i += 1
+
+        self.getAllSubsets(nums, answer, curr_subset, i)
+
+        return answer
+
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        ## TC = O(nlogn) + ( O(n) * O(2^n) ) ~ O(n*2^n)
+        ## SC = O(n) * O(2^n)
+
+        # 1. Sort the array to make all duplicates occur together
+        nums.sort()
+
+        # 2. get all subsets
+        all_subsets = self.getAllSubsets(nums, [], [], 0)
+
+        return all_subsets
