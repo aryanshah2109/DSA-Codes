4e4d6d3870ccf1cbe404a932cfb1167ee60e2e6d
Aryan Shah <aryanrshah2109@gmail.com>
2026-05-09 19:02:01 +0530

    Array revision

--- Arrays/Problems/moveZeroesToEnd.py
+++ Arrays/Problems/moveZeroesToEnd.py
@@ -1,45 +1,43 @@
-arr = [1,2,2,3]
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
 
-n = len(arr)
+        ## Brute
+        ## TC = O(n) SC = O(n)
 
-# Brute Force
-# temp = []
-# for i in range(n):
-#     if(nums[i]!=0):
-#         temp.append(nums[i])
+        # temp = []
+        # for i in range(n):
+        #     if nums[i] != 0:
+        #         temp.append(nums[i])
+
         
-# for i in range(n):
-#     if(i<len(temp)):
-#         nums[i] = temp[i]
-#     else:
-#         nums[i] = 0
-# print(nums)
+        # for i in range(n):
+        #     if i < len(temp):
+        #         nums[i] = temp[i]
+        #     else:
+        #         nums[i] = 0
+            
+        
+        ## Optimal
+        ## TC = O(n) SC = O(1)
+        
+        # Find first zero
+        j = -1
+        for i in range(n):
+            if nums[i] == 0:
+                j = i
+                break
 
-j = -1
+        if j == -1:      # No zero found
+            return
 
-for i in range(n):
-    if(arr[i] == 0):
-        j = i
-        break
- 
-if j == -1: # no zeroes found
-    print(arr)
-    raise SystemExit(1)
-    
-for i in range(j+1,n):
-    if(arr[i] != 0):
-        temp = arr[i]
-        arr[i] = arr[j]
-        arr[j] = temp
-        j+=1
-print(arr)
-
-
-            
-            
-            
-            
-            
-            
-            
-            
+        for i in range(j+1, n):
+            if nums[i] != 0:
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1
+        
+        
